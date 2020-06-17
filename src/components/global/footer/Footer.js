8b460f6f3815a321c743278e4d52cea15659f4ce
MD-REHMAN <Rehman.2468@gmail.com>
2020-06-17 15:57:39 +0530

    Update UI with CSS variable and mobile device optimization

--- src/components/global/footer/Footer.js
+++ src/components/global/footer/Footer.js
@@ -7,12 +7,13 @@ function Footer() {
   return <>
     <footer>
       {/* https://ko-fi.com/md_rehman */}
-      <div className="coffee-icon">
+      {/* <div className="coffee-icon">
         <FaCoffee />
       </div>
       <div className="note">
         If you found this useful and want to support me then you can click here to buy me a coffee 
-      </div>
+      </div> */}
+      Feel free to play around, but it's Alpha build so it'll have lots of bugs.
     </footer>
   </>
 }
